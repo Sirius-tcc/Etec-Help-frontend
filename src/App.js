9f575a4cbe3c4fdb64a9ab98ee587881f1e283e8
Vitor-Carmo <vitorv0071@gmail.com>
2020-09-08 00:24:07 -0300

    first page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css';
+
+
+import Routes from './Routes';
+
+function App() {
+  return (
+      <Routes/>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Logo from '../../assets/images/Logo.svg'
+import Help from '../../assets/images/help.svg'
+import Study from '../../assets/images/study.svg'
+import Teacher from '../../assets/images/teacher-completed.svg'
+
+import { Link } from 'react-router-dom';
+
+
+import './styles.css'
+
+function Landing(){
+    return(
+        <div id="page-landing">
+
+            <div className="logo-container">
+                <img src={Logo} alt="Etec-Help"/>
+                <h2>Sua plataforma de mentoria e compartilhamento de conhecimento</h2>
+            </div>
+            
+            
+            <div className="buttons-container">
+                <Link to="#" className="study">
+                    <img src={Study} alt="Estudar"/>
+                    Estudar
+                </Link>
+
+                <Link to="#" className="help">
+                    <img src={Help} alt="Ajudar"/>
+                    Ajudar
+                </Link>
+            </div>
+
+
+            <img src={Teacher} alt="Plataforma de estudos" className="teacher-image"/>
+
+        </div>
+    );
+}
+
+export default Landing
