44c7aa2788f8caa8dd697eed85e4f76e0dfbac35
Vitor-Carmo <vitorv0071@gmail.com>
2020-09-10 19:45:23 -0300

    adding RegisterHelper route

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import LoginHelper from './pages/LoginHelper';
+import RegisterHelper from './pages/RegisterHelper';
 
 
 
@@ -10,6 +11,7 @@ function Routes(){
         <BrowserRouter>
             <Route path="/" exact component={ Landing } />
             <Route path="/login-helper" exact component={ LoginHelper } />
+            <Route path="/register-helper" exact component={ RegisterHelper } />
         </BrowserRouter>
     );
 }   
