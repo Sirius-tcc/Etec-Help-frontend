1b03ce2f2ecd38a0a3b2815bf3e13545ce3ab921
Vitor-Carmo <vitorv0071@gmail.com>
2020-11-16 01:21:01 -0300

    connecting with the backend and verifying all the fields on the form

--- src/pages/RegisterStudent/index.js
+++ src/pages/RegisterStudent/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import Logo from '../../assets/images/Logo.svg'
 import TableClass from '../../assets/images/table_class.svg'
-
+import { toast } from 'react-toastify';
 import InputLogin from '../../components/InputLogin'
 import Button from '../../components/Button'
 import ImageApresentation from '../../components/ImageApresentation'
@@ -11,8 +11,118 @@ import ImageApresentation from '../../components/ImageApresentation'
 import './styles.css'
 import './responsive.css'
 import './scroll.css'
+import api from '../../services/api';
 
 function RegisterStudent(){
+
+    const [ name, setName ] = useState('')
+    const [ surname, setSurname ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ confirmPassword, setConfirmPassword ] = useState('')
+
+
+    const [ canSend, setCanSend ] = useState(false)
+
+
+    const [ nameError, setNameError ] = useState(false)
+    const [ surnameError, setSurnameError ] = useState(false)
+    const [ emailError, setEmailError ] = useState(false)
+    const [ confirmPasswordError, setConfirmPasswordError ] = useState(false)
+
+    const { push } = useHistory()
+
+    async function handleOnSubmit(e){
+        e.preventDefault()
+
+        if( verifyName(name) ) {
+            setNameError(false)
+
+        } else {
+            toast.error('Nome só pode haver apenas 12 caracteres');
+            setNameError(true)
+            return
+        }
+
+
+        if( verifySurname(surname) ) {
+            setSurnameError(false)
+        } else {
+            toast.error('Sobrenome só pode haver apenas 12 caracteres');
+            setSurnameError(true)
+            return
+        }
+
+        if( verifyEmailValid(email) ) {
+                
+            if( verifyEmailLength(email) ) {
+                setEmailError(false)
+    
+            } else {
+                toast.error('Email Inválido! Apenas pode haver 100 caracteres no máximo!');
+                setEmailError(true)
+            }
+
+        } else {
+            toast.error('Email Inválido!');
+            setEmailError(true)
+            return
+        }
+
+        
+        if( verifyPassword(password, confirmPassword) ) {
+            setConfirmPasswordError(false)
+        } else {
+            toast.error('A confirmação da senha não confere');
+            setConfirmPasswordError(true)
+            return
+        }
+        
+
+        const __data = {
+            name: name.trim(),
+            surname: surname.trim(),
+            email: email.trim(),
+            password: password.trim()
+        }
+
+        const response = await  api.post('/student/create', __data)
+
+        const { data } = response
+
+        if(data.sucess){
+            toast.success('Estudante cadastrado com sucesso!', {
+                autoClose: 2000,
+            });
+            
+            setTimeout(() => { 
+                push('/login-student'); 
+            }, 2000);
+            
+        }else{
+            toast.error('Erro ao cadastrar usuário')
+        }
+    }
+
+
+    useEffect(() => {
+        
+        if(
+            name.trim().length > 0 &&
+            surname.trim().length > 0 &&
+            email.trim().length > 0 &&
+            password.length > 0 &&
+            confirmPassword.length > 0
+        ){
+            setCanSend(true)
+        }else{
+            setCanSend(false)
+        }
+
+    }, [name, surname, email, password, confirmPassword ])
+
+
+
     return(
         <div id="page-register-student">
              <ImageApresentation 
@@ -33,19 +143,45 @@ function RegisterStudent(){
                     <h1>Cadastro</h1>
                     <h2>Preencha os dados abaixo<br/> para começar.</h2>
 
-                    <form>
-                        <InputLogin first name="name" placeholder="Nome"/>
+                    <form onSubmit={ handleOnSubmit }>
+                        <InputLogin 
+                            first name="name" 
+                            placeholder="Nome"
+                            error={ nameError }
+                            onChange= {(e) =>{ setName(e.target.value) }}
+                        />
                         
-                        <InputLogin name="surname" placeholder="Sobrenome"/>
+                        <InputLogin 
+                            name="surname" 
+                            placeholder="Sobrenome"
+                            error={ surnameError }
+                            onChange= {(e) =>{ setSurname(e.target.value) }}
+                        />
                         
-                        <InputLogin name="email" placeholder="E-mail"/>
+                        <InputLogin 
+                            name="email" 
+                            placeholder="E-mail"
+                            error={ emailError }
+                            onChange= {(e) =>{ setEmail(e.target.value) }}
+                        />
                         
-                        <InputLogin name="password" type="password" placeholder="Senha"/>
+                        <InputLogin 
+                            name="password" 
+                            type="password" 
+                            placeholder="Senha"
+                            onChange = {(e) =>{ setPassword(e.target.value) }}
+                        />
                         
-                        <InputLogin last name="confirm password" type="password" placeholder="Confirmar senha"/>
+                        <InputLogin 
+                            last 
+                            name="confirm password" 
+                            type="password" placeholder="Confirmar senha"
+                            error={ confirmPasswordError }
+                            onChange= {(e) =>{ setConfirmPassword(e.target.value) }}
+                        />
 
 
-                        <Button buttonName="Concluir Cadastro"/>
+                        <Button buttonName="Concluir Cadastro" send={canSend} />
 
                     </form>
                 </div>
@@ -56,6 +192,60 @@ function RegisterStudent(){
         </div>
     );
 }
+
+function verifyEmailValid(email){
+
+    if(email.indexOf("@") !== -1 && email.indexOf(".") !== -1){
+        let splitEmail = email.split('@')
+
+        if (splitEmail[0] !== ''){
+            splitEmail = splitEmail[1].split('.')
+            
+            if
+            (
+                splitEmail[0] !== '' &&
+                splitEmail[1] !== '' 
+            ){
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+function verifyEmailLength(email) {
+    if (email.trim().length <= 100){
+        return true
+    }
+
+    return false
+}
+
+function verifyPassword(password1, password2){
+    if (password1 === password2){
+        return true
+    }
+
+    return false
+}
+
+function verifyName(name){
+    if (name.trim().length <= 12){
+        return true
+    }
+
+    return false
+}
+
+function verifySurname(surname){
+    if (surname.trim().length <= 12){
+        return true
+    }
+
+    return false
+}
+
 
 
 export default RegisterStudent;
