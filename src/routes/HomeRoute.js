d8b818aed6b1701e60ae71e2a0691977f4094a55
Vitor-Carmo <vitorv0071@gmail.com>
2020-11-16 01:23:34 -0300

    if the user is logged in and he's on '/', then he goes to the home page

---
+++ src/routes/HomeRoute.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { getTypeUser } from '../scripts/getTokenData'
+import api from '../services/api';
+
+
+
+function HomeRoute(props){
+    const token = localStorage.getItem('app-token')
+
+    const isLogged = !!token
+    
+    const [validToken, setValidToken] = useState(true)
+
+    
+    useEffect(() => {
+        api.get(
+            '/auth/checkAuth', 
+                { headers: 
+                    {
+                        "Authorization" : `bearer ${token}`
+                    } 
+                }
+        ).then(res =>{
+            const { data } = res.data
+            setValidToken(data)
+        });
+    });
+
+
+    if (isLogged) {
+        return validToken ?  <Redirect to={`/${getTypeUser(localStorage.getItem('app-token'))}/home`}/> : <Route { ...props } />
+    }
+    
+    return  <Route { ...props } />
+}
+
+
+export default HomeRoute
